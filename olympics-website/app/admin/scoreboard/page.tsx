20741dc752a42f55aea6dc3cef70105a6ae4ac51
seth-wyzy <swyzyko1@jh.edu>
2026-03-30 19:41:16 -0400

    fixing git issue

---
+++ olympics-website/app/admin/scoreboard/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import prisma from "@/lib/prisma";
+import { useState } from "react";
+
+export default function adminScoreboard{
+    const [selectedGameId, setSelectedGameId] = useState<string>("");
+    const [scores, setScores] = useState<Record<string, number>>({}); 
+    return (
+        1
+    );
+}
+
+export 
