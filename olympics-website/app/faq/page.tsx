46907727c4d1d544ba7e07ab34804fe78e26ce0a
Kent Zhang <kzhang8540@gmail.com>
2026-04-13 19:58:12 -0400

    updated emails on faq page and rules page

--- olympics-website/app/faq/page.tsx
+++ olympics-website/app/faq/page.tsx
@@ -58,7 +58,7 @@ export default function FAQPage() {
                     </a>
                     <p></p>
                     <a
-                        href="mailto:jim@gmail.com"
+                        href="mailto:jhuacmofficers@gmail.com"
                         className="inline-flex items-center text-[#002D72] hover:text-[#001a47] font-semibold focus:outline-none focus:underline"
                     >
                         <svg 
@@ -75,7 +75,7 @@ export default function FAQPage() {
                             d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" 
                             />
                         </svg>
-                        jim@gmail.com
+                        jhuacmofficers@gmail.com
                     </a>
                 </div>
             </div>
--- olympics-website/app/rules/page.tsx
+++ olympics-website/app/rules/page.tsx
@@ -146,7 +146,7 @@ export default function RulesPage() {
               If you have any questions or need clarification on any of these rules, please don't hesitate to reach out to our organizing team.
             </p>
             <a
-              href="mailto:olympics@cs.jhu.edu"
+              href="mailto:jhuacmofficers@gmail.com"
               className="inline-flex items-center text-[#002D72] hover:text-[#001a47] font-semibold focus:outline-none focus:underline"
             >
               <svg 
@@ -163,7 +163,7 @@ export default function RulesPage() {
                   d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" 
                 />
               </svg>
-              olympics@cs.jhu.edu
+              jhuacmofficers@gmail.com
             </a>
           </div>
         </div>
