1ed3d8f9d78330460205def0e6525466a0214f52
seth-wyzy <swyzyko1@jh.edu>
2026-04-27 19:43:11 -0400

    switch event live to false

--- olympics-website/app/scoreboard/page.tsx
+++ olympics-website/app/scoreboard/page.tsx
@@ -13,7 +13,7 @@ export default function ScoreboardPage() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
-    const isEventLive = true;
+    const isEventLive = false;
 
     if (!isEventLive) {
         return (
