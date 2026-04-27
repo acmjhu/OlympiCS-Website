91062eb78c1687897a637165b58749592e3c4034
seth-wyzy <swyzyko1@jh.edu>
2026-04-27 19:44:55 -0400

    Swyzyko1 scoreboard hookup (#62)
    * able to pull admin scoreboard, but not post
    
    * able to use the scoreboard updater
    
    * switch event live to false

--- olympics-website/app/api/admin/scoreboard/route.ts
+++ olympics-website/app/api/admin/scoreboard/route.ts
@@ -15,37 +15,40 @@ export async function POST(req:NextRequest) {
      
 
     const isAdmin =
-        session?.user?.role === "admin" || (session?.user.email !== "jhuacmofficers@gmail.com" && session?.user.email !== "sethwyzy@gmail.com");
+        session?.user?.role === "admin" || session?.user?.email === "jhuacmofficers@gmail.com" || session?.user?.email === "sethwyzy@gmail.com";
    
 
     if (!isAdmin) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
+    
     try {
         const scores: ScoreInput[] = await req.json();
 
-        const upserts = scores.map((item) =>
-        prisma.score.upsert({
-            where: {
-                teamId_eventGamesId: {
-                    teamId: item.teamId,
-                    eventGamesId: item.eventGamesId,
-                },
-            },
-            update: { score: item.scoreValue},
-            create: {
-                teamId: item.teamId,
-                eventGamesId: item.eventGamesId,
-                score: item.scoreValue,
-            },
-            })
-        );
-        await prisma.$transaction(upserts);
+        for (const item of scores) {
+            if (!isNaN(item.scoreValue) && item.teamId && item.eventGamesId) {
+                await prisma.score.upsert({
+                    where: {
+                        teamId_eventGamesId: {
+                            teamId: item.teamId,
+                            eventGamesId: item.eventGamesId,
+                        },
+                    },
+                    update: { score: item.scoreValue },
+                    create: {
+                        teamId: item.teamId,
+                        eventGamesId: item.eventGamesId,
+                        score: item.scoreValue,
+                    },
+                });
+            }
+        }
+            
 
-        return NextResponse.json({success: true});
+            return NextResponse.json({success: true});
     } catch (error) {
-        console.error(error);
+        console.error("Scoreboard Save error: ", error);
         return NextResponse.json( {error: "Failed to save scores"}, {status: 500});
     }
 }
