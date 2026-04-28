e300a574d831101e3092b9c3269136c1ee22ea82
Michellewang375 <michellewang375@gmail.com>
2026-04-27 20:57:41 -0400

    unwanted files

--- olympics-website/app/api/admin/scoreboard/route.ts
+++ olympics-website/app/api/admin/scoreboard/route.ts
@@ -15,7 +15,7 @@ export async function POST(req:NextRequest) {
      
 
     const isAdmin =
-        session?.user?.role === "admin" || (session?.user.email !== "jhuacmofficers@gmail.com" && session?.user.email !== "sethwyzy@gmail.com");
+        session?.user?.role === "admin" || (session?.user.email !== "jhuacmofficers@gmail.com" && session?.user.email !== "sethwyzy@gmail.com" && session?.user.email !== "michellewang375@gmail.com");
    
 
     if (!isAdmin) {
